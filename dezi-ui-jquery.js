85c35ef1ab4def84919b400dc41ad64ad95d99bb
Peter Karman <karpet@peknet.com>
2012-07-13 22:43:48 -0500

    add pager, facets, and stats. TODO more facets work

--- dezi-ui-jquery.js
+++ dezi-ui-jquery.js
@@ -1,40 +1,129 @@
 /* jQuery Dezi::UI example */
 // based on http://nuggets.comperiosearch.com/2011/03/asynchronous-search-results-jquery-solr-json-ajax/
 
-function dezi_search() {
+
+// dynamic file loading from http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml
+function loadjscssfile(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+  var fileref=document.createElement('script')
+  fileref.setAttribute("type","text/javascript")
+  fileref.setAttribute("src", filename)
+ }
+ else if (filetype=="css"){ //if filename is an external CSS file
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", filename)
+ }
+ if (typeof fileref!="undefined")
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+
+function dezi_search(offset) {
+   if (!offset) offset = 0;
    var query = $('#q').get(0).value;
    if (!query || !query.length) {
     return;
    }
    
-   var resDiv = $('#rs').get(0);
-   resDiv.innerHTML = '...Searching...';  // pretty img
-   
-   $.getJSON(DEZI_SEARCH_URI + "?q="+query, function (resp) {
+   var resDiv = $('#results').get(0);
+   resDiv.innerHTML = '<div id="progress">...Searching...<br/><img src="http://dezi.org/ui/example/Progress.gif"/></div>'; 
+   var uri = DEZI_SEARCH_URI + "?q="+query+'&o='+offset;   
+   $.getJSON(uri+'&f=0', function (resp) {
         resDiv.innerHTML = '';
         //console.log(resp);
         for (var i = 0; i < resp.results.length; i++) {
             var res = resp.results[i];
             var r = "<b><a href='" + DEZI_SEARCH_URI + "/" + res.uri + "'>" + res.title + "</a></b><br/>" + res.summary;
             var $d = $('<div class="result">' + r + '</div>');
-            $('#rs').append($d);
+            $('#results').append($d);
         }  
         if (!resp.results.length) {
             resDiv.innerHTML = 'No results';
         }
+        var $stats = $('<div id="stats">'+resp.total+' results | Search time: '+resp.search_time+' | Build time: '+resp.build_time+'</div>');
+        $('#stats').replaceWith($stats);
+        dezi_pager(resp);
     });
+    dezi_facets(uri);
 
+}
+
+function dezi_facets(uri) {
+    var facetsDiv = $('#facets').get(0);
+    facetsDiv.innerHTML = '<div id="fprogress">...Building...<br/><img src="http://dezi.org/ui/example/Progress.gif"/></div>';
+
+    $.getJSON(uri+'&f=1&r=0', function (resp) {
+        facetsDiv.innerHTML = '';
+        console.log(resp);
+        if (!resp.facets) {
+            facetsDiv.innerHTML = 'No facets';
+            return;
+        }
+        for (var facet_name in resp.facets) {
+            var facet = resp.facets[facet_name];
+            var f = facet_name + ' (' + facet.length + ')';
+            var $d = $('<div class="facet">' + f + '</div>');
+            $('#facets').append($d);
+        }
+    });
+}
+
+function dezi_pager(resp) {
+    var pager_html = $('<div id="pager">' +
+      '<a id="pager_m_left"></a><div id="pager_o_left"></div>' +
+      '<div class="paginator_p_wrap">' +
+        '<div class="paginator_p_bloc">' + '</div>' +
+      '</div>' +
+      '<div id="pager_o_right"></div><a id="pager_m_right"></a>' +
+      '<div id="pager_slider" class="paginator_slider" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">' +
+        '<a class="ui-slider-handle ui-state-default ui-corner-all" href="#"></a>' +
+      '</div>' +
+     '</div>');
+
+    if ($('#pager').length) {
+        $('#pager').replaceWith(pager_html);
+    }
+    else {
+        $('#tools').append(pager_html);
+    }
+    var this_page = (resp.offset / resp.page_size) + 1;
+    $("#pager").jPaginator({
+        nbPages: parseInt(resp.total / resp.page_size),
+        selectedPage: this_page,
+        overBtnLeft:'#pager_o_left',
+        overBtnRight:'#pager_o_right',
+        maxBtnLeft:'#pager_m_left',
+        maxBtnRight:'#pager_m_right',
+        onPageClicked: function(a,num) {
+            var new_offset = resp.page_size * (num - 1);
+            dezi_search(new_offset);
+        }
+    });
+     
 }
 
 // generate the page
 $(document).ready(function() {
+    // load helper files
+    loadjscssfile('http://dezi.org/ui/example/jquery-ui-1.8.13.slider.min.js', 'js');
+    loadjscssfile('http://dezi.org/ui/example/jPaginator.js', 'js');
+    loadjscssfile('http://dezi.org/ui/example/jPaginator.css', 'css');
+
+    // if we were called with ?q= then initiate query
     var params = $.deparam.querystring();
     //console.log(params);
     var query = "";
     if (params && params.q) {
         query = params.q.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
     }
-    var $page = $('<div><input type="text" id="q" value="'+query+'"></input><button onclick="dezi_search()">Search</button></div><div id="rs"></div>');
+
+    // generate page structure
+    var $page = $('<div id="tools"><input size="30" type="text" id="q" value="'+query+'"></input>' +
+                   '<button onclick="dezi_search()">Search</button></div>' +
+                  '<div id="stats"></div>' +
+                  '<div id="results"></div><div id="facets"></div>');
     $('body').append($page);
 
     if (query.length) {
